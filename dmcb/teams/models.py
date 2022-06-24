690695d95b557df8bbdc2363aa99a010b6592cfd
sinku <sininoir@gmail.com>
2022-06-24 10:20:49 +0430

    Add base Team model

--- dmcb/teams/models.py
+++ dmcb/teams/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    DIFFICULTY_CHOISES = (
+        ('E', 'Easy'),
+        ('M', 'Medium'),
+        ('H', 'Hard'),
+    )
+    question_id = models.CharField(max_length=30, unique=True, blank=False, primary_key=True)
+    question_text = models.CharField(max_length=200, blank=True)
+    difficulty = models.CharField(choices=DIFFICULTY_CHOISES, max_length=2, blank=False)
+
+    def __str__(self):
+        return self.question_id
