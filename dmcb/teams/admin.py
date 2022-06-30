e3455dba66dc4414b6ac53b83b2df1383ce886f2
sinku <sininoir@gmail.com>
2022-06-30 20:54:11 +0430

    Add participant inline for teams

--- dmcb/teams/admin.py
+++ dmcb/teams/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from teams.models import Team, Participant
 
-# Register your models here.
+
+class ParticipantInline(admin.TabularInline):
+    model = Participant
+
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        ParticipantInline,
+     ]
+
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Participant)
