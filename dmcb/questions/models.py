2a02432a4e72e679e44aa676d1a3ddccb03ff86a
sinku <sininoir@gmail.com>
2022-07-06 20:49:44 +0300

    Add count fields to question

--- dmcb/questions/models.py
+++ dmcb/questions/models.py
@@ -13,6 +13,8 @@ class Question(models.Model):
     )
     question_text = models.CharField(max_length=200, blank=True)
     difficulty = models.CharField(choices=DIFFICULTY_CHOISES, max_length=2, blank=False)
+    solve_count = models.IntegerField(default=0)
+    sell_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question_id
