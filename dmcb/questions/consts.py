ad1632263d2aba3e56b706020c066a4a3345529d
tahafkh <fakharian.taha@ut.ac.ir>
2022-07-07 17:41:41 +0430

    Add 3 views. One left

---
+++ dmcb/questions/consts.py
@@ -0,0 +1,26 @@
+# Questions' prices
+INITIAL_PRICES = {
+    'E': 100,
+    'M': 300,
+    'H': 700,
+    'A': 2000 
+}
+
+# Questions' solving coefficients
+COEFS = {
+    'E': 2,
+    'M': 2,
+    'H': 2,
+    'A': 2.5
+}
+
+# Sell coefficient
+SELL_COEF = 0.5 
+
+# Questions' changing coefficients
+SELL_DELTA = 0.5
+SOLVE_DELTA = 0.25
+
+# Min and max coefficients
+MIN_COEF = 1
+MAX_COEF = 4
