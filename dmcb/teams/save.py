b2b16eb37a006e7bd04886801a5474dd93406593
tahafkh <fakharian.taha@ut.ac.ir>
2022-07-08 15:20:24 +0430

    Save results

--- dmcb/teams/save.py
+++ dmcb/teams/save.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from .models import Team
 
-def get_max():
+def save():
     data = [(i.id, i.balance) for i in Team.objects.all().order_by('balance')]
     results = pd.DataFrame(data, columns=['id', 'balance'])
     results.to_csv('teams/data/results.csv', index=False)
