28581034503ba76542d67552b7f561227b685afd
tahafkh <fakharian.taha@ut.ac.ir>
2022-07-08 15:19:47 +0430

    Get the results

--- dmcb/teams/get_max.py
+++ dmcb/teams/get_max.py
@@ -1,7 +1,10 @@
 from django.db.models import Max
 
+import pandas as pd
+
 from .models import Team
 
 def get_max():
-    for i in Team.objects.all().order_by('balance'):
-        print(i.id, i.balance)
+    data = [(i.id, i.balance) for i in Team.objects.all().order_by('balance')]
+    results = pd.DataFrame(data, columns=['id', 'balance'])
+    results.to_csv('teams/data/results.csv', index=False)
